41ac8415b64d1d0417b12c91c8ec32ecec35772f
branislavstojkovic70 <96660978+branislavstojkovic70@users.noreply.github.com>
2023-07-02 20:41:39 +0200

    Training done 50 epochs

--- RI/main.py
+++ RI/main.py
@@ -18,26 +18,26 @@ import argparse
 import imutils
 import cv2
 # construct the argument parser and parse the arguments
-from keras.saving.saving_api import load_model
+from keras.src.saving.saving_api import load_model
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
+                help="path to input image")
 ap.add_argument("-m", "--model", type=str, required=True,
-	help="path to trained handwriting recognition model")
+                help="path to trained handwriting recognition model")
 # load the handwriting OCR model
 print("[INFO] loading handwriting OCR model...")
 model = load_model("my_model.h5")
 # load the input image from disk, convert it to grayscale, and blur
 # it to reduce noise
-image = cv2.imread("C:\\Users\\ANJA\\Downloads\\model.jpg")
+image = cv2.imread("/Users/bane/Documents/GitHub/RI_projekat/RI/close_button.png")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 # perform edge detection, find contours in the edge map, and sort the
 # resulting contours from left-to-right
 edged = cv2.Canny(blurred, 30, 150)
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
+                        cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 cnts = sort_contours(cnts, method="left-to-right")[0]
 # initialize the list of contour bounding boxes and associated
@@ -45,42 +45,42 @@ cnts = sort_contours(cnts, method="left-to-right")[0]
 chars = []
 # loop over the contours
 for c in cnts:
-	# compute the bounding box of the contour
-	(x, y, w, h) = cv2.boundingRect(c)
-	# filter out bounding boxes, ensuring they are neither too small
-	# nor too large
-	if (w >= 5 and w <= 150) and (h >= 15 and h <= 120):
-		# extract the character and threshold it to make the character
-		# appear as *white* (foreground) on a *black* background, then
-		# grab the width and height of the thresholded image
-		roi = gray[y:y + h, x:x + w]
-		thresh = cv2.threshold(roi, 0, 255,
-			cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-		(tH, tW) = thresh.shape
-		# if the width is greater than the height, resize along the
-		# width dimension
-		if tW > tH:
-			thresh = imutils.resize(thresh, width=28)
-		# otherwise, resize along the height
-		else:
-			thresh = imutils.resize(thresh, height=28)
-# re-grab the image dimensions (now that its been resized)
-		# and then determine how much we need to pad the width and
-		# height such that our image will be 32x32
-		(tH, tW) = thresh.shape
-		dX = int(max(0, 28 - tW) / 2.0)
-		dY = int(max(0, 28 - tH) / 2.0)
-		# pad the image and force 32x32 dimensions
-		padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY,
-			left=dX, right=dX, borderType=cv2.BORDER_CONSTANT,
-			value=(0, 0, 0))
-		padded = cv2.resize(padded, (28, 28))
-		# prepare the padded image for classification via our
-		# handwriting OCR model
-		padded = padded.astype("float32") / 255.0
-		padded = np.expand_dims(padded, axis=-1)
-		# update our list of characters that will be OCR'd
-		chars.append((padded, (x, y, w, h)))
+    # compute the bounding box of the contour
+    (x, y, w, h) = cv2.boundingRect(c)
+    # filter out bounding boxes, ensuring they are neither too small
+    # nor too large
+    if (w >= 5 and w <= 150) and (h >= 15 and h <= 120):
+        # extract the character and threshold it to make the character
+        # appear as *white* (foreground) on a *black* background, then
+        # grab the width and height of the thresholded image
+        roi = gray[y:y + h, x:x + w]
+        thresh = cv2.threshold(roi, 0, 255,
+                               cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        (tH, tW) = thresh.shape
+        # if the width is greater than the height, resize along the
+        # width dimension
+        if tW > tH:
+            thresh = imutils.resize(thresh, width=28)
+        # otherwise, resize along the height
+        else:
+            thresh = imutils.resize(thresh, height=28)
+        # re-grab the image dimensions (now that its been resized)
+        # and then determine how much we need to pad the width and
+        # height such that our image will be 32x32
+        (tH, tW) = thresh.shape
+        dX = int(max(0, 28 - tW) / 2.0)
+        dY = int(max(0, 28 - tH) / 2.0)
+        # pad the image and force 32x32 dimensions
+        padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY,
+                                    left=dX, right=dX, borderType=cv2.BORDER_CONSTANT,
+                                    value=(0, 0, 0))
+        padded = cv2.resize(padded, (28, 28))
+        # prepare the padded image for classification via our
+        # handwriting OCR model
+        padded = padded.astype("float32") / 255.0
+        padded = np.expand_dims(padded, axis=-1)
+        # update our list of characters that will be OCR'd
+        chars.append((padded, (x, y, w, h)))
 # extract the bounding box locations and padded characters
 boxes = [b[1] for b in chars]
 chars = np.array([c[0] for c in chars], dtype="float32")
@@ -88,19 +88,20 @@ chars = np.array([c[0] for c in chars], dtype="float32")
 preds = model.predict(chars)
 # define the list of label names
 labelNames = "0123456789"
-labelNames += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuwxyz"
+labelNames += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 labelNames = [l for l in labelNames]
+
 for (pred, (x, y, w, h)) in zip(preds, boxes):
-	i = np.argmax(pred)
-	prob = pred[i]
-	label = labelNames[i]
-	# draw the prediction on the image
-	print("[INFO] {} - {:.2f}%".format(label, prob * 100))
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-	cv2.putText(image, label, (x - 10, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
-	# show the image
-	cv2.imshow("Image", image)
+    i = np.argmax(pred)
+    prob = pred[i]
+    label = labelNames[i]
+    # draw the prediction on the image
+    print("[INFO] {} - {:.2f}%".format(label, prob * 100))
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.putText(image, label, (x - 10, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
+    # show the image
+    cv2.imshow("Image", image)
 
 cv2.waitKey(0)
 #
@@ -134,7 +135,6 @@ cv2.waitKey(0)
 # model.save("my_model.h5")
 #
 # # Train the model
-# model.fit(train_images, train_labels, epochs=5, batch_size=64)
+# model.fit(train_images, train_labels, epochs=50, batch_size=64)
 #
 # # Don't forget to evaluate the model on your testing samples...
-
