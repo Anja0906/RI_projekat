9c646c1bd97d9ffbee328ca8bba7af985cf10149
branislavstojkovic70 <96660978+branislavstojkovic70@users.noreply.github.com>
2023-07-04 00:02:53 +0200

    added some training data

--- RI/modelDigits.py
+++ RI/modelDigits.py
@@ -14,14 +14,6 @@ BATCH_PER_EPOCH = 256
 VALID_RATIO = 0.2
 DROPOUT = 0 # 0.25
 
-MODELS_DIR = "D:/Fax/DS/DS/Model"
-MODEL_TRAIN_PATH = MODELS_DIR + "model_learn.hdf5"
-MODEL_VALID_PATH = MODELS_DIR + "model_valid.hdf5"
-MODEL_TRAIN_MONITOR = "accuracy"
-MODEL_VALID_MONITOR = "val_accuracy"
-MODEL_MODE = "max"
-HISTORY_PATH = MODELS_DIR + "history.csv"
-
 
 def load_dataset():
     (trainX, trainY), (testX, testY) = mnist.load_data()
@@ -55,24 +47,29 @@ def define_model():
 
 
 def train():
-	trainX, trainY, testX, testY = load_dataset()
-	model = define_model()
-	chkpt_lrn_callback = keras.callbacks.ModelCheckpoint(filepath = MODEL_TRAIN_PATH, save_weights_only = False, monitor = MODEL_TRAIN_MONITOR, mode = MODEL_MODE, save_best_only = True)
-	chkpt_val_callback = keras.callbacks.ModelCheckpoint(filepath = MODEL_VALID_PATH, save_weights_only = False, monitor = MODEL_VALID_MONITOR, mode = MODEL_MODE, save_best_only = True)
-	hist_log_callback = CSVLogger(filename = HISTORY_PATH, append = False)
-	history = model.fit(trainX, trainY, validation_split = VALID_RATIO, epochs = EPOCHS, batch_size = BATCH_SIZE, steps_per_epoch = BATCH_PER_EPOCH, shuffle = True, callbacks = [chkpt_lrn_callback, chkpt_val_callback, hist_log_callback], verbose = 2)
+    trainX, trainY, testX, testY = load_dataset()
+    model = define_model()
+    chkpt_lrn_callback = keras.callbacks.ModelCheckpoint(filepath="model_learn.hdf5", save_weights_only=False,
+                                                         monitor="accuracy", mode="max",
+                                                         save_best_only=True)
+    chkpt_val_callback = keras.callbacks.ModelCheckpoint(filepath="model_valid.hdf5", save_weights_only=False,
+                                                         monitor="val_accuracy", mode="max",
+                                                         save_best_only=True)
+    hist_log_callback = CSVLogger(filename="historydigits.csv", append=False)
+    history = model.fit(trainX, trainY, validation_split=VALID_RATIO, epochs=EPOCHS, batch_size=BATCH_SIZE,
+                        steps_per_epoch=BATCH_PER_EPOCH, shuffle=True,
+                        callbacks=[chkpt_lrn_callback, chkpt_val_callback, hist_log_callback], verbose=2)
 
-	valid_model = keras.models.load_model(MODEL_VALID_PATH)
-	loss = valid_model.evaluate(testX, testY, batch_size = BATCH_SIZE, verbose = 2)
-	predY = model.predict(testX, batch_size = BATCH_SIZE, verbose = 2)
+    valid_model = keras.models.load_model("model_valid.hdf5")
+    loss = valid_model.evaluate(testX, testY, batch_size=BATCH_SIZE, verbose=2)
+    predY = model.predict(testX, batch_size=BATCH_SIZE, verbose=2)
 
-	testY = testY.argmax(axis = 1)
-	predY = predY.argmax(axis = 1)
+    testY = testY.argmax(axis=1)
+    predY = predY.argmax(axis=1)
 
-	cm = confusion_matrix(testY, predY)
+    cm = confusion_matrix(testY, predY)
 
-
-	print(cm)
+    print(cm)
 
 
 if __name__ == "__main__":
